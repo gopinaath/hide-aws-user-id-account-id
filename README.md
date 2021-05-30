6b1973a0f9e5400682409ae3ac33c90dacc46a8b
gopinaath <gopinaath.work@gmail.com>
2021-05-29 22:40:39 -0700

    first commit barebones - tested on chrome only

---
+++ README.md
@@ -0,0 +1,2 @@
+
+Script to hide user name and AWS Account ID / Alias on The top nav bar of AWS console.  Please note that in the current format, this removes the entire navigation features that is underneath this AWS user name and account ID - you won't be able to navigate to "My Account", "My  Organizations", etc.
---
+++ hide-aws-user-id-account-id.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Remove AWS Account ID
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Remove AWS Account ID from AWS Console!
+// @author       You
+// @match        https://console.aws.amazon.com/*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+var username = document.getElementById('nav-usernameMenu');
+if (username) {
+    username.parentNode.removeChild(username);
+}
+    
+})();
