436e9cf74c59157c3274158de241dab064fca044
developer <developer@localhost>
2021-11-30 22:36:08 -0500

    Updated script to be compatible with multiple aws console endpoints, updated multi-line if statement to ternary operator, updated authors, fixed formatting

--- hide-aws-user-id-account-id.js
+++ hide-aws-user-id-account-id.js
@@ -1,19 +1,19 @@
 // ==UserScript==
 // @name         Remove AWS Account ID
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  Remove AWS Account ID from AWS Console!
-// @author       gopinaath
-// @match        https://console.aws.amazon.com/*/*
+// @author       gopinaath, sysdevoperations
+// @match        https://console.aws.amazon.com/*
+// @match        https://console.amazonaws.com/*
+// @match        https://*.console.aws.amazon.com/*
+// @match        https://*.console.amazonaws.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-var username = document.getElementById('nav-usernameMenu');
-if (username) {
-    username.parentNode.removeChild(username);
-}
-    
+    var username = document.getElementById('nav-usernameMenu');
+    username ? username.parentNode.removeChild(username) : '';
 })();
